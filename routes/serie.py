57c212b773226e8d2a885162055ec5b78f444fba
Enrico Bianchi <enrico.bianchi@gmail.com>
2019-10-20 23:32:39 +0200

    Extracted data for GET serie route

--- routes/serie.py
+++ routes/serie.py
@@ -5,15 +5,30 @@
 # Description   A RESTful API for managing a collection of comic books
 # License       GPL version 2 (see LICENSE for details)
 import json
+from contextlib import closing
 
+import psycopg2
 from flask import Response
 
 import utils.database
 
 
 def get_serie():
-    db = utils.database.get_db()
+    data = []
 
-    res = {"msg": "Requested serie", "op": "ok"}
-    resp = Response(json.dumps(res), status=200, mimetype="application/json")
+    try:
+        db = utils.database.get_db()
+
+        with closing(db.cursor()) as cur:
+            cur.execute("SELECT id_serie, nome FROM serie ORDER BY nome")
+            results = cur.fetchall()
+            for item in results:
+                data.append({"id": item[0], "name": item[1]})
+
+        res = {"data": data, "op": "ok"}
+        resp = Response(json.dumps(res), status=200, mimetype="application/json")
+    except psycopg2.OperationalError:
+        res = {"op": "ko", "msg": "Error to connect to database"}
+        resp = Response(json.dumps(res), status=500, mimetype="application/json")
+
     return resp
